7d1757a815b04ce00507cec58a8539c1105e77f0
romaniukV <romaniukv255@gmail.com>
2018-04-03 20:23:26 +0300

    Completed pract7.task1.

--- src/main/java/pract6/SerializationUtils.java
+++ src/main/java/pract6/SerializationUtils.java
@@ -1,5 +1,7 @@
 package pract6;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import pract2.task2.model.entities.Patient;
 import pract2.task2.view.Viewer;
 
@@ -9,13 +11,20 @@ import java.util.List;
 
 public class SerializationUtils {
 
+    private static final Logger logger = LogManager.getLogger(SerializationUtils.class);
+
     public static void saveListToFile(String fileName, List<Patient> patients) {
+        logger.debug("Current file name - " + fileName);
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            logger.info("Saving patients list to file: " + fileName);
             outputStream.writeObject(patients);
             outputStream.flush();
+            logger.info("Saved patients list to file: " + fileName);
         } catch (FileNotFoundException e) {
+            logger.error("Error " + e.getMessage());
             System.out.println(Viewer.FILE_NOT_FOUND);
         } catch (IOException e) {
+            logger.error("Error " + e.getMessage());
             e.printStackTrace();
         }
     }
@@ -23,10 +32,14 @@ public class SerializationUtils {
     public  static List<Patient> readListFromFile(String fileName, List<Patient> patients) {
         List<Patient> result = new ArrayList<>();
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName))) {
+            logger.info("Restoring patients list from file: " + fileName);
             result = (List<Patient>) inputStream.readObject();
+            logger.info("Restored patients list from file: " + fileName);
         } catch (FileNotFoundException e) {
+            logger.error("Error " + e.getMessage());
             System.out.println(Viewer.FILE_NOT_FOUND);
         } catch (IOException | ClassNotFoundException e) {
+            logger.error("Error " + e.getMessage());
             e.printStackTrace();
         }
         return result;
