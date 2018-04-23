431b54683d5641f81acad90fc82fab58f3ff3b8c
romaniukV <romaniukv255@gmail.com>
2018-04-23 19:44:11 +0300

    Start pract10.

--- src/main/java/task10/dao/TaskDAO.java
+++ src/main/java/task10/dao/TaskDAO.java
@@ -18,9 +18,10 @@ public class TaskDAO {
 
     public static List<Staff> selectAllStaff() {
         List<Staff> staffList = new ArrayList<>();
+        ResultSet rs = null;
         try(Connection connection = DBConnection.getConnection()) {
             PreparedStatement ps = connection.prepareStatement(SELECT_ALL_STAFF);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             while (rs.next()) {
                 staffList.add(new Staff(rs.getInt("id"), rs.getString("last_name"), rs.getString("first_name"),
                         rs.getString("position"), rs.getInt("department_id")));
@@ -28,20 +29,39 @@ public class TaskDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return staffList;
     }
 
     public static List<Task> selectAllTasks() {
         List<Task> tasks = new ArrayList<>();
+        ResultSet rs = null;
         try(Connection connection = DBConnection.getConnection()) {
             PreparedStatement ps = connection.prepareStatement(SELECT_ALL_TASKS);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             while (rs.next()) {
                 tasks.add(new Task(rs.getInt("id"), rs.getString("description"),
                         rs.getInt("staff_id")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return tasks;
     }
